40c50b7d8aad710db21ff670954247e2f930a304
henrywallawalla <henry.wallace@menloschool.org>
2021-02-12 13:34:55 -0800

    we have a thing

--- ehtjash.py
+++ ehtjash.py
@@ -4,32 +4,32 @@ import sys
 
 def main():
 	while True: 
-		command = input("$ ")
+		command = input("$ ") #we use input here in the main() for simple commands
 		if command == "exit":
 			break
 		elif command == "help":
-			print_help()
+			prnt_help()
 		elif command[:3] == "cd":
 			ehtjash_cd(command[3:])
 		elif command[:3] == "pwd":
 			ehtjash_pwd()
 		else:
-			command_center(command)
+			command_center(command) #we use sys.argv here for subprocess commands
 
 
-def command_center():
+def command_center(command):
 	pass
 
-def ehtjash_cd(location):
+def ehtjash_cd(location): # doesn't work
 	try:
-		os.chdir() #figure out what goes in here
+		os.chdir(os.path.abspath(location))
 	except Exception:
 		print("ehthash cd: no file or directory found")
 
 def ehtjash_pwd():
 	print(os.getcwd())
 
-def print_help()
+def prnt_help():
 	pass
 
 if __name__ == "__main__":
