6e5cf5fb17ac34dda30c025b08fb7d373ecfbc59
Teeron Hajebi <teeron@teerons-air.lan>
2021-02-25 13:54:26 -0800

    some work

--- ehtjash.py
+++ ehtjash.py
@@ -6,7 +6,11 @@ import sys
 def main():
 
 	user = os.getlogin()
-	response = input("Is this you? " + user)
+	response = input("Is this you? " + user + " ")
+
+	'''
+	pswr = input("Whats the password? ")
+	'''
 
 	while response == "yes": 
 		command = input(ehtjash_pwd() + ": $ ") #we use input here in the main() for simple commands
@@ -14,15 +18,14 @@ def main():
 			break
 		elif command == "help":
 			prnt_help()
-		elif command[:3] == "cd":
+		elif command[:2] == "cd":
 			ehtjash_cd(command[3:])
-		elif command[:3] == "pwd":
+		elif command[:3] == "pwd" || command[:3] == "cwd":
 			print(ehtjash_pwd())
+		elif command[:2] == "ls"
+			ehtjash_ls
 		else:
 			command_center(command) #we use sys.argv here for subprocess commands
-
-
-
 
 def command_center(command):
 	pass
@@ -30,13 +33,20 @@ def command_center(command):
 def ehtjash_cd(location): # doesn't work yet (Path object?)
 	
 	try:
-		os.chdir("/home/henry/Documents")
-	except NotADirecoryError:
+		if location == "":
+			os.chdir("/Users/" + os.getlogin())
+		else:
+			os.chdir(location)
+	except:
 		print("ehthash cd: no file or directory found")
+
+def ehtjash_ls():
+	#Use sys.argv
 
 def ehtjash_pwd():
 	return str(os.getcwd())
 
+#Put commands + descriptions 
 def prnt_help():
 	pass
 
