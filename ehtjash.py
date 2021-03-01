b9db477b7d4ee61ae2a8f8ae29c19c1f736bed76
Henry <henry.wallace@menloschool.org>
2021-03-01 13:59:21 -0800

    we POPEN offgit status

--- ehtjash.py
+++ ehtjash.py
@@ -6,37 +6,39 @@ import argparse
 
 def main():
 
-	user = os.getlogin()
-	response = input("Is this you? " + user + " ")
-
-	'''
-	pswr = input("Whats the password? ")
-	'''
-
-	while response == "yes": 
+	while True: 
 		command = input(ehtjash_pwd() + ": $ ") #we use input here in the main() for simple commands
 		if command == "exit":
 			break
-		elif command == "help":
+		elif command == "--help":
 			prnt_help()
-		elif command[:2] == "cd":
-			ehtjash_cd(command[3:])
-		elif command[:3] == "pwd" or command[:3] == "cwd":
-			print(ehtjash_pwd())
-			'''
-		elif command[:2] == "ls":
-			ehtjash_ls
-			'''
-		else:
-			command_center(command) #we use sys.argv here for subprocess commands
+		command_center(command) 
 
 def command_center(command):
+	
+
+	if command[:3] == "pwd" or command[:3] == "cwd":
+		print(ehtjash_pwd())
+
+	command_list = command.split(" ")
+
+	if command_list[0] == "pwd" or command_list[0] == "cwd":
+		print(ehtjash_pwd())
+	if command_list[0] == "ls":
+		subprocess.Popen(["/usr/bin/ls"] + command_list[1:])
+	elif command_list[0] == "cd":
+		ehtjash_cd(command_list[1])
+
+'''
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--help", dest = "help", type =str, action = "print_help")
 
 	args = parser.parse_args()
 	if (args == "--help"):
 		print(prnt_help())
+'''
+	
+	
 
 def ehtjash_cd(location): # doesn't work yet (Path object?)
 	
@@ -50,6 +52,7 @@ def ehtjash_cd(location): # doesn't work yet (Path object?)
 
 def ehtjash_ls():
 	#Use sys.argv
+	print("not yet")
 
 
 def ehtjash_pwd():
